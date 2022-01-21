589823ade2a4af37728a3372c8c2373780e80e74
shaddam313233 <shaddam343536@gmail.com>
2022-01-21 06:23:57 -0800

    file added

---
+++ js/responsive.js
@@ -0,0 +1,52 @@
+$(document).ready(function(){
+    'use strict';
+    // banner slider;
+    $('.banner-slider').slick({
+       autoplay: true,
+         arrows: false,
+        dots:true,
+    }); 
+    // about venobox;
+    $('.venobox').venobox();
+
+
+    $('.testimonial-slider').slick({
+          arrows: false,
+         dots:true,
+         slidToShow:2,
+     }); 
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
